a9c4983d5042aa938727387e8cc00f72df875832
Garrett Neuenkirchen <garrettn@gmail.com>
2021-01-31 16:45:54 -0600

    adding authentication file to house my secrets. still playing around in main to see if i can use this library.

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ from authentication import *
 from pprint import pprint
 
 stash = stashy.connect(msbbURL, token=msbbToken)
-pprint(stash.projects.list())
+
+# grab AWS project
+aws = next((item for item in stash.projects.list() if item['key'] == 'AWS'))
+stash.projects[aws].repos.list()
+
+print("done")
